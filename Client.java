16353781885e956e546082dbc7591b7f2ac244b9
AKASH DEY <83662743+Akash-msit@users.noreply.github.com>
2021-06-02 01:09:16 +0530

    local host 7777

--- Client.java
+++ Client.java
@@ -2,7 +2,7 @@ import java.io.*;
 import java.net.*; 
 public class Client {
 	public static void main(String[] args)throws Exception {  
-		Socket s=new Socket("localhost",3333);
+		Socket s=new Socket("localhost",7777);
 		DataInputStream din=new DataInputStream(s.getInputStream());  
 		DataOutputStream dout=new DataOutputStream(s.getOutputStream());
 		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
